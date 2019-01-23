0cf376bec8628759af5278fae5375b2c009e9d2c
Kenil <kenilsha93@gmail.com>
2019-01-23 15:38:48 +0530

    Updates

---
+++ precache-manifest.02819b43acf173bda26be95994f20331.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1137e30241298837d770",
+    "url": "/react-form/static/js/runtime~main.1137e302.js"
+  },
+  {
+    "revision": "6d59d009adaf443a7a04",
+    "url": "/react-form/static/js/main.6d59d009.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/react-form/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "6d59d009adaf443a7a04",
+    "url": "/react-form/static/css/main.7deab5b0.chunk.css"
+  },
+  {
+    "revision": "9c78f4e7cb9cf7724019dce4b64ce698",
+    "url": "/react-form/index.html"
+  }
+];
